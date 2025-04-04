b0f90ce35d6a786a6e5997ed6f7fd7f763fc5f92
issaramro <iza04@mail.aub.edu>
2025-04-04 18:48:28 +0300

    Added POST endpoint for batch sentiment analysis

--- app.py
+++ app.py
@@ -17,5 +17,17 @@ def predict_sentiment(input_data: TextInput):
     result = sentiment_model(input_data.text)
     return {"label": result[0]['label'], "score": result[0]['score']}
 
+# barch sentiment analysis
+class BatchTextInput(BaseModel):
+    texts: list[str]
+
+@app.post("/predict/batch/")
+def predict_sentiment_batch(batch_data: BatchTextInput):
+    results = sentiment_model(batch_data.texts)
+    return [
+        {"text": text, "label": res["label"], "score": res["score"]}
+        for text, res in zip(batch_data.texts, results)
+    ]
+
 
 #uvicorn app:app --reload
