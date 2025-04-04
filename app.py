c0a4a1187d4e719ab735ffe259ac24807857f6a8
issaramro <iza04@mail.aub.edu>
2025-04-04 18:51:09 +0300

    Added POST route to compare sentiment of two texts

--- app.py
+++ app.py
@@ -17,5 +17,18 @@ def predict_sentiment(input_data: TextInput):
     result = sentiment_model(input_data.text)
     return {"label": result[0]['label'], "score": result[0]['score']}
 
+# comparing sentiments
+class TextPair(BaseModel):
+    text1: str
+    text2: str
+
+@app.post("/compare/")
+def compare_sentiments(data: TextPair):
+    results = sentiment_model([data.text1, data.text2])
+    return {
+        "text1": {"label": results[0]["label"], "score": results[0]["score"]},
+        "text2": {"label": results[1]["label"], "score": results[1]["score"]}
+    }
+
 
 #uvicorn app:app --reload
